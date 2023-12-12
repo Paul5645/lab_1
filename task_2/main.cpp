dcc0d7648afa6f380a7fac09650e5a8b9bc76b87
Paul5645 <120225770+Paul5645@users.noreply.github.com>
2023-12-13 01:06:01 +0300

    Update main.cpp

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -7,10 +7,10 @@ int main() {
 	unsigned short height = 200;
 	unsigned short weight;
 	cout << "----1----" << "\n";
-	ReadPersonData1(name, age, salary);
+	ReadPersonData(name, age, salary);
 	WritePersonData(name, "200", "60", &age, salary);
 	cout << "----2----" << "\n";
-	ReadPersonData2(name, age, weight, height);
+	ReadPersonData(name, age, weight, height);
 	WritePersonData(name, to_string(weight), to_string(height), &age, 5000);
 	return 0;
 }
